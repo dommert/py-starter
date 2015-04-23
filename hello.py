469477bba3e75a54c74696745a205a6704e62ee4
Dommert <dommertenterprises@gmail.com>
2015-04-23 01:09:46 -0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Python Module
+
+def hello(person):
+    var = 'Hello ' + person + '. Nice to meet you!'
+    return var
+
+def goodbye(person):
+    var = 'Good Bye ' + person + '. See you next time'
+    return var
+
