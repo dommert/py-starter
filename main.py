4a5ae5a92a3458bb7c84834f06a72c944679f1b8
Dommert <dommertenterprises@gmail.com>
2015-04-26 06:26:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 # Main Python File
 
+# Using a import and function
 from hello import hello, goodbye
-
-# One way
-jake = "Jake The Snake"
-print hello(jake)
-
-# Another way
-print hello("Jake The Rabbit")
-
+# Saying Hello
+jake = "Jake The Snake" 
+print hello(jake) #One way
+print hello("Jake The Rabbit")# Another way
+# Saying Goodbye
 print goodbye(jake)
+
+
+
+# Import a class and functions
+from class_student import Student
+
+# initialize 3 Different studnets
+bob = Student("Bob Hope",60, "M")
+dave = Student("David Letterman", 50, "M")
+frank = Student("Frank Miller", 30, "M")
+
+# Say hello to a student
+print Student.hello(frank)
+
+
+
+
